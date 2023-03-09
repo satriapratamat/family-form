1cde83774241e7340f6e939d295971a559430306
Satria Pratama Taruna <satriapratamat7@gmail.com>
2023-03-09 13:30:08 +0700

    Error Handling (Form Validation)

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -16,6 +16,73 @@ export default function FormPage() {
     ]);
     const [errors, setErrors] = useState({});
 
+    //Error Handling
+    const validateForm = () => {
+        const errors = {};
+    
+        if (!name.trim()) {
+          errors.name = 'Name is required';
+        }
+    
+        if (!idNumber.trim()) {
+          errors.idNumber = 'eKTP Number is required';
+        } else if (!/^\d+$/.test(idNumber)) {
+          errors.idNumber = 'eKTP Number must only contain numbers';
+        } else if (idNumber.length !== 16) {
+          errors.idNumber = 'eKTP Number must be 16 digits';
+        }
+    
+        if (!address.trim()) {
+          errors.address = 'Address is required';
+        }
+    
+        if (!job.trim()) {
+          errors.job = 'Job is required';
+        }
+    
+        if (!dob) {
+          errors.dob = 'Date of Birth is required';
+        }
+    
+        const phoneErrors = phoneNumbers.map((phoneNumber, index) => {
+          if (!phoneNumber.number.trim()) {
+            return `Phone Number ${index + 1} is required`;
+          } else if (!/^\d+$/.test(phoneNumber.number)) {
+            return `Phone Number ${index + 1} must only contain numbers`;
+          } else if (phoneNumber.number.length !== 10) {
+            return `Phone Number ${index + 1} must be 10 digits`;
+          }
+          return null;
+        }).filter(error => error !== null);
+    
+        if (phoneErrors.length > 0) {
+          errors.phoneNumbers = phoneErrors;
+        }
+    
+        const familyErrors = familyMembers.map((member, index) => {
+          const memberErrors = {};
+    
+          if (!member.name.trim()) {
+            memberErrors.name = 'Name is required';
+          }
+    
+          if (!member.dob) {
+            memberErrors.dob = 'Date of Birth is required';
+          }
+    
+          if (!member.relationship.trim()) {
+            memberErrors.relationship = 'Relationship is required';
+          }
+    
+          return Object.keys(memberErrors).length > 0 ? { index, errors: memberErrors } : null;
+        }).filter(error => error !== null);
+    
+        if (familyErrors.length > 0) {
+          errors.familyMembers = familyErrors;
+        }
+    
+        return Object.keys(errors).length > 0 ? errors : null;
+      };
     //Handle Input Change
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -52,14 +119,16 @@ export default function FormPage() {
         list[index][name] = value;
         setFamilyMembers(list);
       };
+
+      //Handle Submit
       const handleSubmit = (e) => {
         e.preventDefault();
     
-        /* const errors = validateForm();
+        const errors = validateForm();
         if (Object.keys(errors).length > 0) {
           setErrors(errors);
           return;
-        } */
+        }
     
         const contact = {
           name,
@@ -89,22 +158,27 @@ export default function FormPage() {
                 <label>
                     Name:
                     <input type="text" value={name} name="name" onChange={(e) => handleInputChange(e)} />
+                    {errors.name && <div className="error">{errors.name}</div>}
                 </label>
                 <label>
                     eKTP Number:
                     <input type="text" value={idNumber} name="idNumber" onChange={(e) => handleInputChange(e)} />
+                    {errors.idNumber && <div className="error">{errors.idNumber}</div>}
                 </label>
                 <label>
                     Address:
                     <textarea value={address} name="address" onChange={(e) => handleInputChange(e)} />
+                    {errors.address && <div className="error">{errors.address}</div>}
                 </label>
                 <label>
                     Job:
                     <input type="text" value={job} name="job" onChange={(e) => handleInputChange(e)} />
+                    {errors.job && <div className="error">{errors.job}</div>}
                 </label>
                 <label>
                     Date of Birth:
                     <input type="date" value={dob} name="dob" onChange={(e) => handleInputChange(e)} />
+                    {errors.dob && <div className="error">{errors.dob}</div>}
                 </label>
                 <label>
                     Phone Numbers:
@@ -113,8 +187,13 @@ export default function FormPage() {
                         <input
                             type="text"
                             value={phoneNumber.number}
+                            name={`phoneNumber${index}`}
                             onChange={(event) => handlePhoneChange(index, event)}
                         />
+                        {errors.phoneNumbers &&
+                            errors.phoneNumbers[index] && (
+                                <div className="error">{errors.phoneNumbers[index]}</div>
+                            )}
                         </div>
                     ))}
                     <button type="button" onClick={handleAddPhoneNumber}>
@@ -125,32 +204,70 @@ export default function FormPage() {
                     Family Members:
                     {familyMembers.map((member, index) => (
                     <div key={index}>
-                        <label>Name:</label>
+                        <label>Name:
                         <input
                         type="text"
                         value={member.name}
+                        name={`familyMember${index}Name`}
                         onChange={(event) => handleFamilyChange(index, event)}
-                        name="name"
                         />
-                        <label>Date of Birth:</label>
+                        {/* {errors.familyMembers &&
+                        errors.familyMembers
+                        .filter((error) => error.index === index)
+                        .map((error) => (
+                            <div key={error.index} className="error">
+                            {error.errors.name}
+                            </div>
+                        ))} */}
+                        </label>
+                        <label>Date of Birth:
                         <input
                         type="date"
                         value={member.dob}
+                        name={`familyMember${index}Dob`}
                         onChange={(event) => handleFamilyChange(index, event)}
-                        name="dob"
                         />
-                        <label>Relationship:</label>
+                        {/* {errors.familyMembers &&
+                        errors.familyMembers
+                        .filter((error) => error.index === index)
+                        .map((error) => (
+                            <div key={error.index} className="error">
+                            {error.errors.dob}
+                            </div>
+                        ))} */}
+                        </label>
+                        <label>Relationship:
                         <input
                         type="text"
                         value={member.relationship}
                         onChange={(event) => handleFamilyChange(index, event)}
                         name="relationship"
                         />
+                        {/* {errors.familyMembers &&
+                        errors.familyMembers
+                        .filter((error) => error.index === index)
+                        .map((error) => (
+                            <div key={error.index} className="error">
+                            {error.errors.dob}
+                            </div>
+                        ))} */}
+                        </label>
                     </div>
                     ))}
                     <button type="button" onClick={handleAddFamilyMember}>
                         Add Family Member
                     </button>
+                    {errors.familyMembers && errors.familyMembers.length > 0 && (
+                        <ul className="errorList">
+                            {errors.familyMembers.map((error) => (
+                                <li key={error.index}>
+                                    {Object.values(error.errors).map((message) => (
+                                        <div key={message}>{message}</div>
+                                    ))}
+                                </li>
+                            ))}
+                        </ul>
+                    )}
                 </label>
                 <button type="submit">Submit</button>
             </form>
